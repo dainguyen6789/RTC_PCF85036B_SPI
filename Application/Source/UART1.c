74433e9bb377846abebb2a6c1d47ca3f5a507c98
Dai Nguyen <dai@localhost>
2018-05-02 15:17:20 -0400

    Correct Display (second)  to UART1

---
+++ Application/Source/UART1.c
@@ -0,0 +1,74 @@
+#include "stc15f2k60s2.h"
+#define TBAUD (65536-FOSC/4/BAUD)
+#define FOSC 18432000L
+#define BAUD 115200
+bit busy;
+
+
+void Uart() interrupt 4 using 1
+{
+	if(RI) 
+	{
+		RI=0;
+	}
+	if(TI)
+	{
+		TI=0;
+		busy=0;
+	}
+}
+void initUART1(void)
+{
+	SCON=0x50; //0101 0000 8-bit uart,  baud rate variable
+	AUXR |=0x95;//10010101 Timer2 as Baud Rate generator
+	T2L=TBAUD;
+	T2H=TBAUD>>8;	
+	ES=1; 					// enable uart1 interrupt
+	EA=1;						// each interrupt source will be enable or disable by setting its interrupt bit
+	PS=0; 					// Serial Port 1 interrupt priority control bit, DS page 561
+}
+
+void SendUART1(unsigned char dat)
+{
+	while(busy);
+	busy=1;
+	ACC=dat;
+	SBUF=ACC;
+}
+
+
+
+ void SendString(char *s)
+{
+	while(*s)
+	{
+		SendUART1(*s++);
+	}
+}
+unsigned char ten(unsigned char BCD)
+{
+	return (BCD)>>4;
+}
+unsigned char unit(unsigned char BCD)
+{
+
+	//unit=BCD&0x0f;
+	//dec=ten*10+unit;
+	return BCD&0x0f;
+}
+unsigned char DectoBCD(unsigned char dec)
+{
+/*	unsigned char ten,unit;
+	ten=BCD>>4;
+	unit=BCD&0x0f;
+	//dec=ten*10+unit;
+	return ten*10+unit;*/
+	return dec;
+}
+//if dat=1, we need to use ASCII value to display "1" on UART1
+unsigned char ASCII_Letter(unsigned char dat)
+{
+	return dat+48;
+}
+
+ 
