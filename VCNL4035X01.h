60bde10cbf2bd425882c9823dec6bfe0c46664a9
Dai Nguyen <dai@localhost>
2018-06-07 10:31:43 -0400

    Add VNCL4035 dot h file

---
+++ VCNL4035X01.h
@@ -0,0 +1,25 @@
+#define VCNL4035_addr 0x60
+
+#define ALS_CONF1 0x00
+#define ALS_CONF2 0x00
+
+#define ALS_THDH_L 0x01
+#define ALS_THDH_M 0x01
+
+#define ALS_THDL_L 0x02
+#define ALS_THDL_M 0x02
+
+#defined PS_CONF1  0x03
+#defined PS_CONF2  0x03
+
+#defined PS_CONF3  0x04
+#defined PS_MS	   0x04
+
+#defined PS_CANC_L 0x05
+#defined PS_CANC_M 0x05
+
+#defined PS_THDL_L 0x05
+#defined PS_THDL_M 0x05
+
+#defined PS_THDH_L 0x06
+#defined PS_THDH_M 0x07
