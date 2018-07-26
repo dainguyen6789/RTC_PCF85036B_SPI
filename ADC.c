74a4afe35e1e518ead5fed510d97d28ca36253c9
Dai Nguyen <dai@localhost>
2018-07-26 17:05:19 -0400

    add ADC: not test yet

---
+++ ADC.c
@@ -0,0 +1,20 @@
+#include "ADC.h"
+#include "stc15f2k60s2.h"
+#include "LCD_Driver_SPLC780D.h"
+
+void adc_isr() interrupt 5 using 1
+{
+	
+	ADC_CONTR&=!ADC_Flag;
+	adc_data=ADC_RES;// 8 high bit
+	adc_data=(adc_data<<2)|ADC_RESL;//ADC 10 bit
+}
+
+void initADC()
+{
+	P1ASF=P1ASF|(1<<0);// P1.0 is ADC port
+	ADC_RES=0;
+	ADC_RESL=0;
+	ADC_CONTR=ADC_PWR | ADC_START;
+	Wait_ms(2);
+}
---
+++ Application/Header/ADC.h
@@ -0,0 +1,4 @@
+#define ADC_START  	1<<3
+#define ADC_PWR  		1<<7
+#define ADC_Flag 		0x10
+extern int adc_data;
