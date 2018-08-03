f7e483fdfd20fedd1029e6d51452c905c0e344b9
Dai Nguyen <dai@localhost>
2018-08-03 17:26:47 -0400

    human error of previous commit

--- BipolarMotor.c
+++ BipolarMotor.c
@@ -90,7 +90,7 @@ void Update_position(unsigned char mnths,unsigned char dys,unsigned char hurs,un
 
 float trend_line_30_31(float time)
 {
-	return -4.88*time*time+126.08*time-745.26-(1.1*5+1.1*4)/2;
+	return -4.88*time*time+126.08*time-745.26-(1.1*4.5);
 
 }
 //
