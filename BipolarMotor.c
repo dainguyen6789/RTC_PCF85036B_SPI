4ca2e2db958af9171874f8662f7e402a8bc7f51d
Dai Nguyen <dai@localhost>
2018-08-21 16:42:05 -0400

    work with JP declination and PSA alg. from http://www.psa.es/sdg/sunpos.htm Need to verify the calculation again
    The algo. is from 12PM to 17PM (corrected sun time).

--- BipolarMotor.c
+++ BipolarMotor.c
@@ -2,6 +2,7 @@
 #include "LCD_Driver_SPLC780D.h"
 #include "Receiver_Position_Data.h"
 #include "math.h"
+#include "SunPos.h"
 //#include "PCF85963BTL.h"
 void Delay_ms(unsigned int ms);
 int Day_Of_Year(unsigned char months,unsigned char days);
@@ -82,72 +83,15 @@ float  findDet4x4 (float  a11, float  a12, float  a13, float  a14,
 		
 //=====================================================	
 
-float
-cubic_interpolate (float *p1, float *p2, float *p3, float *p4, float x)
-{
-  float a, b, c, d, det;	// the parameters of the cubic function
-  float a11, a12, a13, a14, a21, a22, a23, a24,
-    a31, a32, a33, a34, a41, a42, a43, a44, k1, k2, k3, k4;
 
-  a11 = pow (p1[0], 3);
-  a21 = pow (p2[0], 3);
-  a31 = pow (p3[0], 3);
-  a41 = pow (p4[0], 3);
-
-  a12 = pow (p1[0], 2);
-  a22 = pow (p2[0], 2);
-  a32 = pow (p3[0], 2);
-  a42 = pow (p4[0], 2);
-
-  a13 = pow (p1[0], 1);
-  a23 = pow (p2[0], 1);
-  a33 = pow (p3[0], 1);
-  a43 = pow (p4[0], 1);
-
-  a14 = 1;
-  a24 = 1;
-  a34 = 1;
-  a44 = 1;
-
-  k1 = p1[1];
-  k2 = p2[1];
-  k3 = p3[1];
-  k4 = p4[1];
-
-
-  det = findDet4x4 (a11, a12, a13, a14, a21, a22, a23, a24,
-		    a31, a32, a33, a34, a41, a42, a43, a44);
-		    if(det!=0)
-		    {
-                  a = findDet4x4 (k1, a12, a13, a14, k2, a22, a23, a24,
-                		  k3, a32, a33, a34, k4, a42, a43, a44) / det;
-                
-                  b = findDet4x4 (a11, k1, a13, a14, a21, k2, a23, a24,
-                		  a31, k3, a33, a34, a41, k4, a43, a44) / det;
-                
-                  c = findDet4x4 (a11, a12, k1, a14, a21, a22, k2, a24,
-                		  a31, a32, k3, a34, a41, a42, k4, a44) / det;
-                  d = findDet4x4 (a11, a12, a13, k1, a21, a22, a23, k2,
-                		  a31, a32, a33, k3, a41, a42, a43, k4) / det;
-                //return det;	  
-        		  return (a*125)*pow(x/5,3)+(b*25)*pow(x/5,2)+c*x+d;
-		    }
-		    else
-		    {
-		        return 0;
-		    }
-
-    //return a21;
-  
-}
 //=====================================================
-float  linear_interpolate(float  x1,float  x2,float  y1,float  y2, float  x)
+float  linear_interpolate(struct point p1,struct point p2, float  x)
 {
-	float  a,b;
-	a=(y1-y2)/(x1-x2);
-	b=y1-a*x1;
+	float  fx;
 	
-	return a*x+b;
+	fx=-(p1.x-x)/(p1.x-p2.x)*(p1.y-p2.y)+p1.y;
+	
+	return fx;
 }
 
 
@@ -156,130 +100,68 @@ void Update_position(unsigned char mnths,unsigned char dys,
 										 float  *currnt_pos)
 {
 	unsigned int date,i,yy;
-	float  desired_distance,distance=0;
-	float  pos_interpolate_12_17h[21][1],p1[2],p2[2],p3[2],p4[2],current_time;
-	/*hurs=0x12;
-	mns=0x30;*/
-	//sconds=0x15;
+	float  desired_distance,distance=0,JP_pos;
+	float  pos_interpolate_12_17h[num_of_time_stamp],current_time;
+	float declination;
+	struct point p1,p2;
+	struct cTime time;
+	struct cLocation location;
+	struct cSunCoordinates *sunCoord;
+	hurs=hurs-1;// change to sun time
+	time.iYear=2018;
+	time.iMonth=BCDtoDec1(mnths);
+	time.iDay=BCDtoDec1(dys);
+	time.dHours=BCDtoDec1(hurs);
+	time.dMinutes=BCDtoDec1(mns);
+	time.dSeconds=BCDtoDec1(sconds&0x7f);
+
+	
 	desired_distance=*currnt_pos;
-	date=Day_Of_Year(mnths,dys);
-	date=87;
-	current_time=12;
-	if (date>=79 && date <=171)
-	{
-		// interpolate for day
-		for (i=0;i<8;i++)
-		{
-			if( (date >=date_stamp_79_171[i]) && (date <= date_stamp_79_171[i+1]))
-			{
-				if (i==0)// 79 to 87
-				{
-					p1[0]=date_stamp_79_171[0];
-					p2[0]=date_stamp_79_171[1];
-					p3[0]=date_stamp_79_171[2];
-					p4[0]=date_stamp_79_171[3];
-					for (yy=0;yy<21;yy++)
-					{
-						p1[1]=exact_rx_pos_simulation[yy][0];
-						p2[1]=exact_rx_pos_simulation[yy][1];
-						p3[1]=exact_rx_pos_simulation[yy][2];
-						p4[1]=exact_rx_pos_simulation[yy][3];
-						// this will give the interpolated position of "date" at time stamp defined in Time_stamp_PM[21][2]
-						pos_interpolate_12_17h[yy][1]=cubic_interpolate(p1,p2,p3,p4,date);
-					}
-					//break;
-				}
-				
-			
-			else if (i==7)// 152 to 171
-			{
-					p1[0]=date_stamp_79_171[i+1];
-					p2[0]=date_stamp_79_171[i];
-					p3[0]=date_stamp_79_171[i-1];
-					p4[0]=date_stamp_79_171[i-2];
-					for (yy=0;yy<21;yy++)
-					{
-						p1[1]=exact_rx_pos_simulation[yy][i+1];
-						p2[1]=exact_rx_pos_simulation[yy][i];
-						p3[1]=exact_rx_pos_simulation[yy][i-1];
-						p4[1]=exact_rx_pos_simulation[yy][i-2];
-						// this will give the interpolated position of "date" at time stamp defined in Time_stamp_PM[21][2]
-						pos_interpolate_12_17h[yy][1]=cubic_interpolate(p1,p2,p3,p4,date);
-					}	
-					//break;
-			}
-			else
-			{
-					p1[0]=date_stamp_79_171[i+2];
-					p2[0]=date_stamp_79_171[i+1];
-					p3[0]=date_stamp_79_171[i];
-					p4[0]=date_stamp_79_171[i-1];
-					for (yy=0;yy<21;yy++)
-					{
-						p1[1]=exact_rx_pos_simulation[yy][i+2];
-						p2[1]=exact_rx_pos_simulation[yy][i+1];
-						p3[1]=exact_rx_pos_simulation[yy][i];
-						p4[1]=exact_rx_pos_simulation[yy][i-1];
-						// this will give the interpolated position of "date" at time stamp defined in Time_stamp_PM[21][2]
-						pos_interpolate_12_17h[yy][1]=cubic_interpolate(p1,p2,p3,p4,date);
-					}
-					//break;
-			}
-			break;
-		}		
-	}
-		// interpolate for hour
-		for(i=0;i<21;i++)
-		{
-			if (current_time>=Time_stamp_PM[i] && current_time<=Time_stamp_PM[i+1])
-			{
-				if(i==0)//7 to 7h30
-				{
-					p1[0]=Time_stamp_PM[i];
-					p2[0]=Time_stamp_PM[i+1];
-					p3[0]=Time_stamp_PM[i+2];
-					p4[0]=Time_stamp_PM[i+3];
+	//date=Day_Of_Year(mnths,dys);
+	date=237;
+	declination=sunpos(time,location,&sunCoord)*180/pi;
+	
+	current_time=(float) BCDtoDec1(hurs)+(float)BCDtoDec1(mns)/60+(float)BCDtoDec1(sconds&0x7f)/3600;
 
-					p1[1]=pos_interpolate_12_17h[i][1];
-					p2[1]=pos_interpolate_12_17h[i+1][1];
-					p3[1]=pos_interpolate_12_17h[i+2][1];
-					p4[1]=pos_interpolate_12_17h[i+3][1];
-						// this will give the interpolated position of "date" at time stamp defined in Time_stamp_PM[21][2]
-					desired_distance=cubic_interpolate(p1,p2,p3,p4,current_time);
-				}
-				else if (i==20)// 16.5h to 17h
+	// interpolate for day
+		for (i=0;i<num_of_day_stamp;i++)
+		{
+			if (declination<=date_declination_mapping[i] && declination>=date_declination_mapping[i+1])
+			{
+				for (yy=0;yy<num_of_time_stamp;yy++)
 				{
-					p1[0]=Time_stamp_PM[i];
-					p2[0]=Time_stamp_PM[i+1];
-					p3[0]=Time_stamp_PM[i-1];
-					p4[0]=Time_stamp_PM[i-2];
-
-					p1[1]=pos_interpolate_12_17h[i][1];
-					p2[1]=pos_interpolate_12_17h[i+1][1];
-					p3[1]=pos_interpolate_12_17h[i-1][1];
-					p4[1]=pos_interpolate_12_17h[i-2][1];
-						// this will give the interpolated position of "date" at time stamp defined in Time_stamp_PM[21][2]
-					desired_distance=cubic_interpolate(p1,p2,p3,p4,current_time);
+					p1.x=date_declination_mapping[i];
+					p2.x=date_declination_mapping[i+1];
 					
-				}
-				else // from 7h30 to 16h30
-				{
-					p1[0]=Time_stamp_PM[i];
-					p2[0]=Time_stamp_PM[i+1];
-					p3[0]=Time_stamp_PM[i+2];
-					p4[0]=Time_stamp_PM[i-1];
-
-					p1[1]=pos_interpolate_12_17h[i][1];
-					p2[1]=pos_interpolate_12_17h[i+1][1];
-					p3[1]=pos_interpolate_12_17h[i+2][1];
-					p4[1]=pos_interpolate_12_17h[i-1][1];
-						// this will give the interpolated position of "date" at time stamp defined in Time_stamp_PM[21][2]
-					desired_distance=cubic_interpolate(p1,p2,p3,p4,current_time);
+					p1.y=RX_pos[yy][i];
+					p2.y=RX_pos[yy][i+1];
+					
+					pos_interpolate_12_17h[yy]=linear_interpolate(p1,p2,declination);
 				}
 				break;
 			}
 		}
-	}
+
+		// interpolate for hour
+		for(i=0;i<num_of_time_stamp;i++)
+		{
+			if(current_time>=Time_stamp_PM[i]&&current_time<=Time_stamp_PM[i+1] && BCDtoDec1(sconds&0x7f)%30==0)
+			{
+				p1.x=Time_stamp_PM[i];
+				p2.x=Time_stamp_PM[i+1];
+				
+				p1.y=pos_interpolate_12_17h[i];
+				p2.y=pos_interpolate_12_17h[i+1];
+				
+				JP_pos=linear_interpolate(p2,p1,current_time);
+				break;
+				
+			}
+			
+		}
+		
+	desired_distance=179+2*JP_pos;
+		
 	distance=desired_distance-*currnt_pos;
 	
 	if(distance>0)
@@ -291,18 +173,3 @@ void Update_position(unsigned char mnths,unsigned char dys,
 	return;
 
 }
-// return 4 closest points of "point"
-/*void closest_points(float  input[],float  *closest_pts, float  point)
-{
-	int i;
-	for(i=0;i<21;i++)
-	{
-		if (point>=input[i] && point<=input[i+1] )
-		{
-			closest_pts[0]=input[i];
-			closest_pts[1]=input[i+1];
-		}
-		
-	}
-	return;
-}*/
