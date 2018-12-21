c1d289afbd7c73c2c5566d34535ed1889eb059db
Dai Nguyen <dai@localhost>
2018-12-21 13:24:06 -0500

    change header file for the PCBA of Datawind/ISP

--- ADCh.h
+++ ADCh.h
@@ -1,11 +1,14 @@
 #define ADC_Stable_Threshold 30
 #define ADC_START  	1<<3
-#define ADC_PWR  		1<<7
+#define ADC_PWR  		0x80
 #define ADC_Flag 		0x10
 
 #define P1_0_ADC_FUNCTION 	0x01
+#define P1_1_ADC_FUNCTION 	0x02
 #define ADC_FLAG     	0x10
+#define ADC_SPEEDLL 0x00
 //#define P1_0_INPUT_P1M0 0x00
 //#define P1_0_INPUT_P1M0 0x01
 
 extern int adc_data;
+//extern int  calib_mode=1;
--- Application/Source/ADC.c
+++ Application/Source/ADC.c
@@ -2,26 +2,29 @@
 #include "ADCh.h"
 #include "stc15f2k60s2.h"
  #include <intrins.h>
+ void Wait_ms(int ms);
+
 void ADC_Init(void)
 {
 	// P1.0 is input
 	P1M1 |=( 1<<0);  
 	P1M0 &=~( 1<<0); 
-	
+	CLK_DIV=0x00;
 	P1ASF |=P1_0_ADC_FUNCTION;  
 		
 	ADC_RES=0;	  		
-					
-	ADC_CONTR=ADC_PWR;	//enable ADC power	
-	//Delay_ms(2);	   
+	ADC_CONTR=ADC_PWR | ADC_SPEEDLL;				
+	//ADC_CONTR=ADC_PWR;	//enable ADC power	
+	Wait_ms(2);
+	
 }
 
 
-unsigned int ADC_GetResult(unsigned char ch) // unsigned int is 16 bit data
+unsigned int ADC_GetResult(unsigned char ch)
 {
 	unsigned int ADC_Value;
-	ADC_CONTR =ADC_PWR  | ch | ADC_START;	
-  _nop_();                     
+	ADC_CONTR =ADC_PWR | ADC_SPEEDLL | ch | ADC_START;	
+  _nop_();                        
   _nop_();
   _nop_();
   _nop_();
@@ -29,13 +32,5 @@ unsigned int ADC_GetResult(unsigned char ch) // unsigned int is 16 bit data
   ADC_CONTR &= ~ADC_FLAG;         
 	ADC_Value=ADC_RES;							
 	ADC_Value=(ADC_Value<<2)|ADC_RESL;
-	_nop_();                     
-  _nop_();
-  _nop_();
-  _nop_();  
-	_nop_();                     
-  _nop_();
-  _nop_();
-  _nop_();
   return ADC_Value;               	  
 }
