687ec42220dd0fea05999122b7eb69821f33e194
Dai Nguyen <dai@localhost>
2019-02-08 17:55:00 -0500

    Add Code for SPI NOR FLash

---
+++ Application/Header/AT25SF041.h
@@ -0,0 +1,22 @@
+#include "stc15f2k60s2.h"
+
+#define  Byte_Page_Program 	0x02
+#define  Read_Array 				0x03
+#define  Write_Enable 			0x06h
+//The PCF85063BTL contains 18 8-bit registers with an auto-incrementing register address, 
+ sbit 			AT25SF041_CS	=	P4^1;	   
+ sbit 			AT25SF041_SCK	=	P4^2;	   
+ sbit 			AT25SF041_SDI	=	P4^3;	   
+ sbit 			AT25SF041_SDO	=	P4^4;	   
+ 
+ #define		AT25SF041_CS_Clr()			{AT25SF041_CS=0;} // Clear CS signal
+ #define		AT25SF041_CS_Set()			{AT25SF041_CS=1;} // Set CS signal
+
+ #define		AT25SF041_SCK_Clr()		{AT25SF041_SCK=0;} 
+ #define		AT25SF041_SCK_Set()		{AT25SF041_SCK=1;}
+
+ #define		AT25SF041_SDI_Clr()		{AT25SF041_SDI=0;} 
+ #define		AT25SF041_SDI_Set()		{AT25SF041_SDI=1;}
+
+ #define		AT25SF041_SDO_Clr()		{AT25SF041_SDO=0;} 
+ #define		AT25SF041_SDO_Set()		{AT25SF041_SDO=1;}
---
+++ Application/Source/AT25SF041.c
@@ -0,0 +1,86 @@
+#include "AT25SF041.h"
+
+void AT25SF041_WriteEnable(void)
+{
+	AT25SF041_CS_Clr();
+	for(int i=0;i<8;i++)
+	{
+			AT25SF041_SCK_Clr();
+			AT25SF041_SDO=(Write_Enable<<i)&0x01;
+			AT25SF041_SCK_Set();
+	}
+	AT25SF041_CS_Clr();
+}
+
+
+void AT25SF041_Write(char opcode, unsigned long int addr,char dat)
+{
+	AT25SF041_CS_Clr();
+	//////////////////////////////////////////
+	for(int i=0;i<8;i++)
+	{
+			AT25SF041_SCK_Clr();
+			AT25SF041_SDO=(opcode<<i)&0x01;
+			AT25SF041_SCK_Set();
+	}
+	
+		for(int i=0;i<23;i++)
+	{
+			AT25SF041_SCK_Clr();
+			AT25SF041_SDO=(addr<<i)&0x01;
+			AT25SF041_SCK_Set();
+	}
+	
+		for(int i=0;i<8;i++)
+	{
+			AT25SF041_SCK_Clr();
+			AT25SF041_SDO=(dat<<i)&0x01;
+			AT25SF041_SCK_Set();
+	}
+	///////////////////////////////////////
+	AT25SF041_CS_Set();
+
+	
+}
+
+char  AT25SF041_Read((char opcode,unsigned long int addr)
+{
+	char dat=0;
+	AT25SF041_CS_Clr();
+	//////////////////////////////////////////
+	// Send the OPCODE
+	for(int i=0;i<8;i++)
+	{
+			AT25SF041_SCK_Clr();
+			AT25SF041_SDO=(opcode<<i)&0x01;
+			AT25SF041_SCK_Set();
+	}
+	// SEND THE ADDR (24-bit)
+		for(int i=0;i<23;i++)
+	{
+			AT25SF041_SCK_Clr();
+			AT25SF041_SDO=(addr<<i)&0x01;
+			AT25SF041_SCK_Set();
+	}
+	/////////////////////////////////////////
+	// Store the output data from NOR FLash
+	for(int i=0;i<8;i++)
+	{
+			AT25SF041_SCK_Clr();
+			if(AT25SF041_SDI)
+			{
+				// INPUT BIT =1
+				dat|=1<<i;
+			}
+			else
+			{
+				// INPUT BIT =0
+				dat&=~(1<<i);
+			}
+			AT25SF041_SCK_Set();
+	}	
+	
+	///////////////////////////////////////
+	AT25SF041_CS_Set();	
+
+}
