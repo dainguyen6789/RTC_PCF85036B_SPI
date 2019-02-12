af04308937682bcbbd8598ffd84107ad727514a1
Dai Nguyen <dai@localhost>
2019-02-12 13:32:31 -0500

    SPI NOR does not work

--- Application/Header/AT25SF041.h
+++ Application/Header/AT25SF041.h
@@ -5,8 +5,8 @@
 #define  Write_Enable 			0x06
 //The PCF85063BTL contains 18 8-bit registers with an auto-incrementing register address, 
  sbit 			AT25SF041_CS	=	P4^1;	   
- sbit 			AT25SF041_SCK	=	P4^2;	   
- sbit 			AT25SF041_SDI	=	P4^3;	   
+ sbit 			AT25SF041_SCK	=	P4^3;	   
+ sbit 			AT25SF041_SDI	=	P4^2;	   
  sbit 			AT25SF041_SDO	=	P4^4;	   
  
  #define		AT25SF041_CS_Clr()			{AT25SF041_CS=0;} // Clear CS signal
@@ -24,5 +24,5 @@
 void AT25SF041_WriteEnable(void);
 void AT25SF041_Write(char opcode, unsigned long int addr,char dat);
 char  AT25SF041_Read(char opcode,unsigned long int addr);
- void Wait_ms_i2c(int ms);
+void Wait_ms_i2c(int ms);
 
--- Application/Source/AT25SF041.c
+++ Application/Source/AT25SF041.c
@@ -3,20 +3,33 @@
 void AT25SF041_WriteEnable(void)
 {
 	int i;
-	AT25SF041_CS_Clr();
-Wait_ms_i2c(2);
+	char write_en;
+	write_en=Write_Enable;
+//	AT25SF041_CS_Clr();
+//Wait_ms_i2c(2);
 	
-	for(i=0;i<8;i++)
+	for( i=0;i<8;i++)
 	{
-			AT25SF041_SCK_Clr();
-			Wait_ms_i2c(2);
-			AT25SF041_SDO=(Write_Enable<<i)&0x01;
-			AT25SF041_SCK_Set();
-			Wait_ms_i2c(2);
+				AT25SF041_SCK_Clr();
+				Wait_ms_i2c(2);
+				if( (write_en&0x80)==0x80 )	  	 //
+				{
+						AT25SF041_SDI_Set();
+				}	
+					else
+				{
+						AT25SF041_SDI_Clr();
+
+				}
+				Wait_ms_i2c(2); 		
+				AT25SF041_SCK_Set();
+				Wait_ms_i2c(2);
+				write_en <<= 1;                   //
+
 
 	}
-	AT25SF041_CS_Set();
-	Wait_ms_i2c();
+	//AT25SF041_CS_Set();
+	//Wait_ms_i2c();
 	
 }
 
@@ -25,26 +38,47 @@ void AT25SF041_Write(char opcode, unsigned long int addr,char dat)
 {
 	int i;
 	AT25SF041_CS_Clr();
-Wait_ms_i2c(2);
+	Wait_ms_i2c(2);
 
 	//////////////////////////////////////////
 	for( i=0;i<8;i++)
-	{
-			AT25SF041_SCK_Clr();
-		Wait_ms_i2c(2);
-			AT25SF041_SDO=(opcode<<i)&0x01;
-			AT25SF041_SCK_Set();
-		Wait_ms_i2c(2);
-		
-	}
-	
-		for( i=0;i<23;i++)
+		{
+				AT25SF041_SCK_Clr();
+				Wait_ms_i2c(2);
+				if( (opcode&0x80)==0x80 )	  	 //
+				{
+						AT25SF041_SDI_Set();
+				}	
+					else
+				{
+						AT25SF041_SDI_Clr();
+
+				}
+				Wait_ms_i2c(2); 		
+				AT25SF041_SCK_Set();
+				Wait_ms_i2c(2);
+				opcode <<= 1;                   //
+
+
+		}
+	// SEND THE ADDR (24-bit)
+	for( i=0;i<24;i++)
 	{
 			AT25SF041_SCK_Clr();
 			Wait_ms_i2c(2);
-			AT25SF041_SDO=(addr<<i)&0x01;
+			if( (addr&0x80)==0x80 )	  	 //
+			{
+					AT25SF041_SDI_Set();
+			}	
+				else
+			{
+					AT25SF041_SDI_Clr();
+
+			}
+			Wait_ms_i2c(2); 		
 			AT25SF041_SCK_Set();
-		Wait_ms_i2c(2);
+			Wait_ms_i2c(2);
+			addr <<= 1;
 
 	}
 	
@@ -52,14 +86,24 @@ Wait_ms_i2c(2);
 	{
 			AT25SF041_SCK_Clr();
 			Wait_ms_i2c(2);
-			AT25SF041_SDO=(dat<<i)&0x01;
+			if( (dat&0x80)==0x80 )	  	 //
+			{
+					AT25SF041_SDI_Set();
+			}	
+				else
+			{
+					AT25SF041_SDI_Clr();
+
+			}
+			Wait_ms_i2c(2); 		
 			AT25SF041_SCK_Set();
 			Wait_ms_i2c(2);
+			dat <<= 1;
 
 	}
 	///////////////////////////////////////
-	AT25SF041_CS_Set();
-	Wait_ms_i2c(2);
+	//AT25SF041_CS_Set();
+	//Wait_ms_i2c(2);
 	
 }
 
@@ -76,40 +120,62 @@ char  AT25SF041_Read(char opcode,unsigned long int addr)
 	{
 			AT25SF041_SCK_Clr();
 			Wait_ms_i2c(2);
-			AT25SF041_SDO=(opcode<<i)&0x01;
+			if( (opcode&0x80)==0x80 )	  	 //
+			{
+					AT25SF041_SDI_Set();
+			}	
+				else
+			{
+					AT25SF041_SDI_Clr();
+
+			}
+			Wait_ms_i2c(2); 		
 			AT25SF041_SCK_Set();
 			Wait_ms_i2c(2);
+			opcode <<= 1;                   //
+
 
 	}
 	// SEND THE ADDR (24-bit)
-	for( i=0;i<23;i++)
+	for( i=0;i<24;i++)
 	{
 			AT25SF041_SCK_Clr();
 			Wait_ms_i2c(2);
-			AT25SF041_SDO=(addr<<i)&0x01;
+			if( (addr&0x80)==0x80 )	  	 //
+			{
+					AT25SF041_SDI_Set();
+			}	
+				else
+			{
+					AT25SF041_SDI_Clr();
+
+			}
+			Wait_ms_i2c(2); 		
 			AT25SF041_SCK_Set();
 			Wait_ms_i2c(2);
+			addr <<= 1;
 
 	}
 	/////////////////////////////////////////
 	// Store the output data from NOR FLash
+	AT25SF041_SDO_Set();
 	for( i=0;i<8;i++)
 	{
-			AT25SF041_SCK_Clr();
-		Wait_ms_i2c(2);
-
-			if(AT25SF041_SDI)
-			{
-				// INPUT BIT =1
-				dat|=1<<i;
-			}
-			else
-			{
-				// INPUT BIT =0
-				dat&=~(1<<i);
-			}
+			dat <<= 1;	              //
 			AT25SF041_SCK_Set();
 			Wait_ms_i2c(2);
+			AT25SF041_SCK_Clr();
+			Wait_ms_i2c(2);
+
+			if(AT25SF041_SDO)
+			{
+				// INPUT BIT =1
+				dat |= 0x01;            //
+			}
+
+			//Wait_ms_i2c(2); 
+			//AT25SF041_SCK_Set();
+			//Wait_ms_i2c(2);
 
 	}	
 	
