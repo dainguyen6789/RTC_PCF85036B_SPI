db6f0458c80e69c0e25d344d223c22bf915e549f
Dai Nguyen <dai@localhost>
2019-02-22 12:37:06 -0500

    prev code does not work

--- Application/Source/AT25SF041.c
+++ Application/Source/AT25SF041.c
@@ -342,12 +342,12 @@ void SPI_NOR_Write_Data(struct data_to_store dat,unsigned long int *addr)
 	AT25SF041_Write(Byte_Page_Program, 6,dat.calib_max_pos_float);
 	Wait_ms_SPINOR(50);	
 	
-	/*AT25SF041_WriteEnable();
+	AT25SF041_WriteEnable();
 	//Wait_ms_SPINOR(50);	
 	AT25SF041_Write(Byte_Page_Program, 7,dat.light_ADC);	
 	Wait_ms_SPINOR(50);	
 
-	AT25SF041_WriteEnable();
+	/*AT25SF041_WriteEnable();
 	//Wait_ms_SPINOR(50);
 	AT25SF041_Write(Byte_Page_Program, 8,dat.Voltage_at_LUT_pos);	
 	Wait_ms_SPINOR(50);	
