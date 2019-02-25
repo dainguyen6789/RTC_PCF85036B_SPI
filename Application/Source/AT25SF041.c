aee8482d33095b2ef8710d582d77a1f78e665cd0
Dai Nguyen <dai@localhost>
2019-02-25 17:58:09 -0500

    ADDRESS BUG

--- Application/Source/AT25SF041.c
+++ Application/Source/AT25SF041.c
@@ -114,7 +114,7 @@ void AT25SF041_Write(unsigned char opcode, unsigned long int addr,unsigned char 
 	{
 			AT25SF041_SCK_Clr();
 			Wait_ms_SPINOR(5);
-			if( (addr&0x80)==0x80 )	  	 //
+			if( (addr&0x00800000)==0x00800000 )	  	 //
 			{
 					AT25SF041_SDI_Set();
 			}	
@@ -190,7 +190,7 @@ unsigned char  AT25SF041_Read(unsigned char opcode,unsigned long int addr)
 	{
 			AT25SF041_SCK_Clr();
 			Wait_ms_SPINOR(5);
-			if( (addr&0x00000080)==0x00000080 )	  	 //
+			if( (addr&0x00800000)==0x00800000 )	  	 //
 			{
 					AT25SF041_SDI_Set();
 			}	
