c7c808a68c0b0d490586fa64d960e64273cf6ad3
Dai Nguyen <dai@localhost>
2019-03-20 13:44:29 -0400

    Gear ratio 27 and offset 71mm from JP Zero position

--- BipolarMotor.c
+++ BipolarMotor.c
@@ -83,7 +83,7 @@ void Move(float  distance, bit direction)
 {
 		unsigned int step;
 		//step= (unsigned int)(distance*337.5/3.14159);//
-		step= (unsigned int)(distance*97);
+		step= (unsigned int)(distance*120);
 		Step_move(step,direction);
 }
 //=====================================================
@@ -224,7 +224,7 @@ void Update_position(unsigned char mnths,unsigned char dys,
 			
 		}
 		
-		desired_distance=JP_pos+150+offset_calib;
+		desired_distance=JP_pos+71+offset_calib;
 		//desired_distance=azimuth;
 		distance=desired_distance-*currnt_pos;
 		if(abs(distance)>=0.5&& desired_distance>=-15 &&desired_distance<=210 )// move if the change is more than 0.5mm
