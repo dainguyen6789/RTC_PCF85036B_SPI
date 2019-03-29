2c51f4d890832836cf457da87bf7f1ba12114447
Dai Nguyen <dai@localhost>
2019-03-29 10:43:11 -0400

    io good for lcd

--- Application/Header/LCD_Driver_SPLC780D.h
+++ Application/Header/LCD_Driver_SPLC780D.h
@@ -6,11 +6,20 @@
 #define Entry_Mode_Set 0x06
 #define Function_Set_8bit 0x38
 #define Function_Set_4bit	0x18
- sbit 			LCD_E	=	P1^3;	 
- sbit 			LCD_RS	=	P1^1;	   
- sbit 			LCD_RW	=	P1^2;	   
- sbit 			LCD_D4	=	P1^4;	   
- sbit 			LCD_D5	=	P1^5;	
- sbit 			LCD_D6	=	P1^6;	
- sbit 			LCD_D7	=	P1^7;	 
+// sbit 			LCD_E	=	P1^3;	 
+// sbit 			LCD_RS	=	P1^1;	   
+// sbit 			LCD_RW	=	P1^2;	   
+// sbit 			LCD_D4	=	P1^4;	   
+// sbit 			LCD_D5	=	P1^5;	
+// sbit 			LCD_D6	=	P1^6;	
+// sbit 			LCD_D7	=	P1^7;	 
+ 
+ sbit 			LCD_E	=	P3^3;	 
+ sbit 			LCD_RS	=	P4^0;	   
+ sbit 			LCD_RW	=	P3^2;	   
+ sbit 			LCD_D4	=	P3^4;	   
+ sbit 			LCD_D5	=	P3^5;	
+ sbit 			LCD_D6	=	P3^6;	
+ sbit 			LCD_D7	=	P3^7; 
+ 
  void Wait_ms(int ms);
